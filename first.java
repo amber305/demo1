04451d236d0696dfc159e053cfc93ab44edea12c
Yash2758 <yashdodani600@gmail.com>
2025-04-15 11:00:18 +0530

    dubara phele bar kiya he

---
+++ first.java
@@ -0,0 +1,30 @@
+// package myfirst;
+
+import java.util.*;
+
+public class first {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int testcase = sc.nextInt();
+        while(testcase>0){
+            // int sta = sc.nextInt();
+            String brac = sc.next();
+            Stack<Character> st = new Stack<>();
+
+            for(int i = 0; i<brac.length(); i++){
+                char ch = brac.charAt(i);
+                if(ch=='('){
+                    st.push('(');
+                }else if(!st.isEmpty()){
+                    st.pop();
+                }
+                
+            }
+            testcase--;
+            System.out.println(st.size());
+
+        }
+    }
+
+   
+}
